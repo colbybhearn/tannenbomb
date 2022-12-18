d70f1cc10eed019101f222eb636a8899e4c3c8f3
colbybhearn <colbybhearn@gmail.com>
2022-12-18 00:49:56 -0600

    site

---
+++ tb/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+     <div>Christmas Tree burning is a Hearn family tradition.</div>
+
+     <h2>TannenBomb Recipee</h2>
+     <ul>
+      <li>1 very dry Christmas tree</li>
+      <li>$100 of fireworks</li>
+      <li>8 ounces lighter fluid</li>
+      <li>Remote detonators (optional)</li>
+     </ul>
+
+     <h2>Do you dislike Christmas?</h2>
+     <div>No, actually we love Christmas and Jesus, a lot.</div>
+
+     <h2></h2>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+}
+</style>
---
+++ tb/src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <div>
+      <h1>🎄TannenBomb 2022 </h1>
+      Fairwell Christmas tree and Hello '23!
+
+      <p>Firework Show and Christmas Tree Burning <br/>
+      Come enjoy a hot cocoa bar and Christmas music at 7pm on New Year's Eve </p>
+
+      <br/>
+      <div id="please">Please:</div>
+      <div>📲 Let us know that you're coming!</div>
+      <div>❄️ Bring chairs and blankets</div>
+      <div>🚗 Do <i>not</i> park in front yards</div>
+
+      
+      
+      <p>10862 Lost Creek, 
+        <br />
+        Whitehouse TX, 75791</p>
+      <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3375.6202360797915!2d-95.2345455849031!3d32.21446228114558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8648329b53d1631b%3A0x4e48b7b1d53a65c3!2s10862%20Lost%20Creek%2C%20Whitehouse%2C%20TX%2075791!5e0!3m2!1sen!2sus!4v1671342926269!5m2!1sen!2sus" width="600" height="450" style="border:0;margin: auto" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+      
+
+      <h2>Accepting donations for <a href="https://axiacenter.org/">Axia Pregnancy Center</a></h2>
+      https://venmo.com/u/ChelseyAlyse<br/>
+      <img src="./../assets/venmoqr.png" style="width:25%" >
+      
+
+      
+
+      <div>TannenBomb - 2020</div>
+      
+    </div>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+
+
+<style>
+#please{
+  position: relative;
+    left: -200px;
+    top: 30px;
+    transform: rotate(-35deg);
+    color: green; 
+    font-size: 24px
+}
+</style>
