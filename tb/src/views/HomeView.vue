606450190f9a31ef95b5cb8c3e5be9a3434ca2d2
colbybhearn <colbybhearn@gmail.com>
2022-12-18 01:15:49 -0600

    refining layout and content

--- tb/src/views/HomeView.vue
+++ tb/src/views/HomeView.vue
@@ -2,34 +2,44 @@
   <div class="home">
     <div>
       <h1>🎄TannenBomb 2022 </h1>
-      Fairwell Christmas tree and Hello '23!
+      Farewell Christmas tree & Welcome '23!
 
       <p>Firework Show and Christmas Tree Burning <br/>
-      Come enjoy a hot cocoa bar and Christmas music at 7pm on New Year's Eve </p>
+      Come enjoy a hot cocoa bar and Christmas music at 7pm on New Year's Eve.</p>
+
+      <section id="schedule">
+        <span>Schedule</span>
+      <ul>
+        <li>6:00 Hot cocoa bar opens</li>
+        <li>6:55 Pre-Tree Mortar Shells</li>
+        <li>7:00 Firework show and tree burning</li>
+        <li>7:10 The End</li>
+      </ul>
+      </section>
 
       <br/>
-      <div id="please">Please:</div>
-      <div>📲 Let us know that you're coming!</div>
-      <div>❄️ Bring chairs and blankets</div>
-      <div>🚗 Do <i>not</i> park in front yards</div>
+      <section id="please">
+      <span >Please be aware</span>
+      <ul>
+        <li>📲 Let us know that you're coming!</li>
+      <li>❄️ Bring chairs and blankets, ear protection for young'ins (it gets loud!) </li>
+      <li>🚗 Do <i>not</i> park in front yards</li>
+          </ul></section>
 
       
-      
-      <p>10862 Lost Creek, 
+      <p>10862 Lost Creek 
         <br />
         Whitehouse TX, 75791</p>
       <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3375.6202360797915!2d-95.2345455849031!3d32.21446228114558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8648329b53d1631b%3A0x4e48b7b1d53a65c3!2s10862%20Lost%20Creek%2C%20Whitehouse%2C%20TX%2075791!5e0!3m2!1sen!2sus!4v1671342926269!5m2!1sen!2sus" width="600" height="450" style="border:0;margin: auto" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
       
 
-      <h2>Accepting donations for <a href="https://axiacenter.org/">Axia Pregnancy Center</a></h2>
+      <h2>100% of donations benefit <br/><a href="https://axiacenter.org/">Axia Pregnancy Center</a></h2>
       https://venmo.com/u/ChelseyAlyse<br/>
       <img src="./../assets/venmoqr.png" style="width:25%" >
       
 
-      
-
-      <div>TannenBomb - 2020</div>
+      <H1>TannenBomb 2020</H1>
+      <img src="./../assets/2020 flame.jpeg" style="width:50%">
       
     </div>
     
@@ -48,12 +58,21 @@ export default {
 
 
 <style>
-#please{
-  position: relative;
-    left: -200px;
-    top: 30px;
-    transform: rotate(-35deg);
-    color: green; 
-    font-size: 24px
+
+
+section {
+  width: fit-content;
+    margin: auto;
 }
+
+section span{
+  text-align: left;
+    display: block;
+}
+
+ul {
+  text-align: left;
+}
+
+
 </style>
